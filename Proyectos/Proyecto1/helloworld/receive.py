20e5fb3c539dda110e1247b714665a300db23ac0
portatil jjsanchezc <jjsanchezc@eafit.edu.co>
2023-03-21 10:06:50 -0500

    añadi la capacidad de que pueda enviar o recibir

--- Proyectos/Proyecto1/helloworld/receive.py
+++ Proyectos/Proyecto1/helloworld/receive.py
@@ -1,16 +1,19 @@
 import socket
 
-def receive_messages():
+def receive_messages(message=None):
     # Creamos un socket y nos conectamos al servidor MOM
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('localhost', 8000))
 
     # Recibimos los mensajes enviados a través del servidor MOM
-    while True:
-        data = client_socket.recv(1024)
-        if not data:
-            break
-        print('Mensaje recibido:', data.decode())
+    if not message:
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            print('Mensaje recibido:', data.decode())
+    else:
+        client_socket.sendall(message.encode())
 
     # Cerramos la conexión
     client_socket.close()
