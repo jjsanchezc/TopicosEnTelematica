5304bef965f20301d0a98f9dfc555725e5bfe174
jjsanchezc <jjsanchezc@eafit.edu.co>
2023-03-23 17:47:47 -0500

    añadi los setters y getters de los topic

--- Proyectos/Proyecto1/MOM/topics.py
+++ Proyectos/Proyecto1/MOM/topics.py
@@ -1,3 +1,10 @@
 class Topics:
-    def __init__(self,name):
+    def __init__(self,name=None):
         self.name=name
+    
+    def set_topic(self,name):
+        self.name=name
+    
+    def get_name(self):
+        print(f'este es el nombre del topico {self.name}')
+        return str(self.name)
